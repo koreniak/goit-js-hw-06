40d045e21b38529bd295d15dc3c538b55fe759f7
Borys Koreniak <koreniak.borys@gmail.com>
2023-03-25 18:31:58 +0200

    Виправив завдання №6

--- js/task-06.js
+++ js/task-06.js
@@ -1,9 +1,12 @@
 const inputRef = document.querySelector("#validation-input");
 
 function controlLength(event) {
-    event.currentTarget.value.length === Number(inputRef.dataset.length) ?
-    inputRef.classList.add('valid') :
+if (event.currentTarget.value.length === Number(inputRef.dataset.length)) {
+    inputRef.classList.add('valid');
+    inputRef.classList.remove('invalid');
+} else {
     inputRef.classList.add('invalid');
-}
+    inputRef.classList.remove('valid');
+}};
 
 inputRef.addEventListener('blur', controlLength);
